e1f90ff387b8b3eb7466eee5cd8c59102f82bfee
Jacob Clarke <jacobclarke92@gmail.com>
2024-07-07 21:11:05 +1000

    calibration sketch update

--- src/sketches/01_Calibration.ts
+++ src/sketches/01_Calibration.ts
@@ -18,7 +18,78 @@ export default class Calibration extends Sketch {
     initPen(this)
     plotBounds(this)
 
-    //
+    const offset = 0
+
+    /**
+     * Horizontal lines
+     */
+    for (let i = 0; i < 10; i++) {
+      this.ctx.beginPath()
+      this.ctx.moveTo(offset + 10, offset + (i + 1) * 10 - 2)
+      this.ctx.lineTo(offset + 10, offset + (i + 1) * 10 + 2)
+      this.ctx.lineTo(offset + 10, offset + (i + 1) * 10 + 2)
+      this.ctx.stroke()
+      this.ctx.closePath()
+    }
+
+    for (let i = 0; i < 10; i++) {
+      this.ctx.beginPath()
+      this.ctx.moveTo(offset + this.cw / 2 - i * 12, offset + (i + 1) * 10 - 2)
+      this.ctx.lineTo(offset + this.cw / 2 - i * 12, offset + (i + 1) * 10 + 2)
+      this.ctx.lineTo(offset + this.cw / 2 - i * 12, offset + (i + 1) * 10 - 2)
+      this.ctx.stroke()
+      this.ctx.closePath()
+    }
+
+    for (let i = 0; i < 10; i++) {
+      this.ctx.beginPath()
+      this.ctx.moveTo(offset + 10, offset + (i + 1) * 10)
+      this.ctx.lineTo(offset + this.cw / 2 - i * 12, offset + (i + 1) * 10)
+      this.ctx.stroke()
+      this.ctx.closePath()
+    }
+
+    /**
+     * Vertical lines
+     */
+
+    for (let i = 0; i < 10; i++) {
+      this.ctx.beginPath()
+      this.ctx.moveTo(offset + this.cw - (i + 1) * 10 - 2, offset + 10)
+      this.ctx.lineTo(offset + this.cw - (i + 1) * 10 + 2, offset + 10)
+      this.ctx.lineTo(offset + this.cw - (i + 1) * 10 - 2, offset + 10)
+      this.ctx.stroke()
+      this.ctx.closePath()
+    }
+
+    for (let i = 0; i < 10; i++) {
+      this.ctx.beginPath()
+      this.ctx.moveTo(offset + this.cw - (i + 1) * 10 - 2, offset + this.ch - 16 * (i + 1))
+      this.ctx.lineTo(offset + this.cw - (i + 1) * 10 + 2, offset + this.ch - 16 * (i + 1))
+      this.ctx.lineTo(offset + this.cw - (i + 1) * 10 - 2, offset + this.ch - 16 * (i + 1))
+      this.ctx.stroke()
+      this.ctx.closePath()
+    }
+
+    for (let i = 0; i < 10; i++) {
+      this.ctx.beginPath()
+      this.ctx.moveTo(offset + this.cw - (i + 1) * 10, offset + 10)
+      this.ctx.lineTo(offset + this.cw - (i + 1) * 10, offset + this.ch - 16 * (i + 1))
+      this.ctx.stroke()
+      this.ctx.closePath()
+    }
+
+    const rectSize = 2
+    const rectGrow = 2
+    const gap = 1
+    for (let i = 0; i < 10; i++) {
+      this.ctx.strokeRect(
+        offset + 10 + i * (rectGrow + i * gap),
+        offset + this.ch - 10 - rectSize - i * rectGrow,
+        rectSize + i * rectGrow,
+        rectSize + i * rectGrow
+      )
+    }
 
     penUp(this)
   }
