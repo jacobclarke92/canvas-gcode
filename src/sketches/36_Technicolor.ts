ebd728824cca5c8e495d4aeecf4d29a538edd6ae
Jacob Clarke <jacob@bigfish.tv>
2024-08-03 20:53:19 +1000

    first few initial sketches

--- src/sketches/36_Technicolor.ts
+++ src/sketches/36_Technicolor.ts
@@ -1,3 +1,4 @@
+import type { IBodyDefinition, IChamferableBodyDefinition } from 'matter-js'
 import { Bodies, Body, Composite, Engine, Vector } from 'matter-js'
 
 import Point from '../Point'
@@ -28,31 +29,57 @@ export default class Technicolor extends Sketch {
       min: 10,
       max: 10000,
       step: 1,
+      disableRandomize: true,
     })
     this.addVar('objects', {
-      initialValue: 3,
+      initialValue: 1,
       min: 1,
       max: 25,
       step: 1,
+      disableRandomize: true,
     })
-
-    this.addVar('minSize', {
-      initialValue: 5,
-      min: 1,
-      max: 25,
-      step: 1,
+    this.addVar('initialX', {
+      initialValue: 0.5,
+      min: 0,
+      max: 1,
+      step: 0.001,
     })
-    this.addVar('maxSize', {
-      initialValue: 20,
-      min: 1,
-      max: 50,
-      step: 1,
+    this.addVar('initialY', {
+      initialValue: 0.25,
+      min: 0,
+      max: 1,
+      step: 0.001,
     })
-    this.addVar('sizeOscAmount', {
+    this.addVar('initialAngle', {
+      initialValue: 0.24,
+      min: 0,
+      max: Math.PI / 2,
+      step: 0.001,
+    })
+    this.addVar('initialTorque', {
       initialValue: 0.1,
       min: 0,
       max: 1,
       step: 0.001,
+    })
+    this.addVar('minSize', {
+      initialValue: 35,
+      min: 1,
+      max: 75,
+      step: 1,
+    })
+    this.addVar('maxSize', {
+      initialValue: 40,
+      min: 1,
+      max: 100,
+      step: 1,
+    })
+    this.addVar('sizeOscAmount', {
+      initialValue: 0,
+      min: 0,
+      max: 0.25,
+      step: 0.001,
+      disableRandomize: true,
     })
     this.addVar('sizeOscSpeed', {
       initialValue: 0.1,
@@ -67,21 +94,21 @@ export default class Technicolor extends Sketch {
     })
     this.addVar('initSpeedX', {
       initialValue: 0.005,
-      min: -0.02,
-      max: 0.02,
-      step: 0.001,
+      min: -0.1,
+      max: 0.1,
+      step: 0.0001,
     })
     this.addVar('initSpeedY', {
       initialValue: -0.005,
-      min: -0.02,
-      max: 0.02,
+      min: -0.1,
+      max: 0.1,
       step: 0.001,
     })
     this.addVar('bounciness', {
       initialValue: 0.95,
       min: 0,
       max: 1,
-      step: 0.01,
+      step: 0.0001,
     })
     this.addVar('simSpeed', {
       initialValue: 10,
@@ -94,20 +121,25 @@ export default class Technicolor extends Sketch {
       min: 0.1,
       max: 50,
       step: 0.1,
+      disableRandomize: true,
     })
     this.vs.showWalls = new BooleanRange({
+      disableRandomize: true,
+      initialValue: false,
+    })
+    this.vs.circleInstead = new BooleanRange({
       disableRandomize: true,
       initialValue: false,
     })
   }
 
   engine: Engine
-  boxes: Matter.Body[] = []
+  shapes: Matter.Body[] = []
   panicCount = 0
   finalized = false
 
   colors = ['#00ff00', '#ff00ff', '#00ffff', '#ff0000'] as const
-  colorShapes: Record<number, Vector[][]>
+  colorShapes: Record<number, (Vector[] | { pos: Vector; radius: number })[]>
 
   initDraw(): void {
     this.ctx.ctx.fillStyle = '#111111'
@@ -124,21 +156,47 @@ export default class Technicolor extends Sketch {
 
     this.colorShapes = { 0: [], 1: [], 2: [], 3: [] }
     this.engine = Engine.create({ enableSleeping: true, velocityIterations: 12 })
-    this.boxes = []
+    this.shapes = []
     this.panicCount = 0
     this.finalized = false
 
-    const { objects, minSize, maxSize, initSpeedX, initSpeedY, bounciness, wallThickness } =
-      this.vars
+    const {
+      objects,
+      minSize,
+      maxSize,
+      initSpeedX,
+      initSpeedY,
+      bounciness,
+      wallThickness,
+      initialAngle,
+      initialX,
+      initialY,
+      initialTorque,
+    } = this.vars
+
+    const isCircle = !!this.vs.circleInstead.value
 
     for (let i = 0; i < objects; i++) {
       const size = randFloatRange(maxSize, minSize)
-      const box = Bodies.rectangle(this.cw / 4, this.ch / 3, size, size, {
+
+      const shapeProperties: IBodyDefinition = {
         restitution: bounciness,
-        angle: randFloatRange(Math.PI / 2),
+        // friction: isCircle ? 0.9 : 0.1,
+        density: 0.0001,
+        angle: initialAngle,
+        // angularSpeed
+        torque: initialTorque,
         force: { x: initSpeedX, y: initSpeedY },
-      })
-      this.boxes.push(box)
+      }
+
+      const x = initialX * this.cw
+      const y = initialY * this.ch
+
+      if (isCircle) {
+        this.shapes.push(Bodies.circle(x, y, size / 2, shapeProperties))
+      } else {
+        this.shapes.push(Bodies.rectangle(x, y, size, size, shapeProperties))
+      }
     }
 
     const ground = Bodies.rectangle(
@@ -146,22 +204,26 @@ export default class Technicolor extends Sketch {
       this.ch - wallThickness / 2,
       this.cw,
       wallThickness,
-      { isStatic: true }
+      { isStatic: true, restitution: 1, friction: 0.05 }
     )
     const roof = Bodies.rectangle(this.cw / 2, wallThickness / 2, this.cw, wallThickness, {
       isStatic: true,
+      restitution: 1,
+      friction: 0.05,
     })
     const wallLeft = Bodies.rectangle(wallThickness / 2, this.ch / 2, wallThickness, this.ch, {
       isStatic: true,
+      restitution: 1,
+      friction: 0.05,
     })
     const wallRight = Bodies.rectangle(
       this.cw - wallThickness / 2,
       this.ch / 2,
       wallThickness,
       this.ch,
-      { isStatic: true }
+      { isStatic: true, restitution: 1, friction: 0.05 }
     )
-    Composite.add(this.engine.world, [ground, roof, wallLeft, wallRight, ...this.boxes])
+    Composite.add(this.engine.world, [ground, roof, wallLeft, wallRight, ...this.shapes])
 
     if (this.vs.showWalls.value) {
       this.drawBody(ground)
@@ -193,7 +255,13 @@ export default class Technicolor extends Sketch {
       if (i > 0) stopAndWigglePen(this, `color ${color}`)
       this.ctx.strokeStyle = color
       for (const shape of this.colorShapes[i]) {
-        this.drawVertices(shape)
+        if (!!this.vs.circleInstead.value && !Array.isArray(shape)) {
+          this.ctx.beginPath()
+          this.ctx.strokeCircle(shape.pos.x, shape.pos.y, shape.radius)
+          this.ctx.closePath()
+        } else {
+          this.drawVertices(shape as Vector[])
+        }
       }
     }
     penUp(this)
@@ -203,7 +271,7 @@ export default class Technicolor extends Sketch {
   draw(increment: number): void {
     const { startAfter } = this.vars
 
-    const bedtime = this.boxes.every((box) => box.isSleeping)
+    const bedtime = this.shapes.every((box) => box.isSleeping)
     if (bedtime || ++this.panicCount >= this.vars.stopAfter) {
       this.finalizeDraw()
       return
@@ -211,7 +279,7 @@ export default class Technicolor extends Sketch {
 
     this.sizeOsc.step(increment)
 
-    for (const box of this.boxes) {
+    for (const box of this.shapes) {
       Body.scale(box, 1 + this.sizeOsc.value.x, 1 + this.sizeOsc.value.x)
     }
 
@@ -220,8 +288,15 @@ export default class Technicolor extends Sketch {
     if (increment < startAfter) return
     const colorIndex = increment % this.colors.length
 
-    for (const box of this.boxes) {
-      this.colorShapes[colorIndex].push(box.vertices.map((vector) => Vector.clone(vector)))
+    for (const shape of this.shapes) {
+      if (!!this.vs.circleInstead.value) {
+        this.colorShapes[colorIndex].push({
+          pos: Vector.clone(shape.position),
+          radius: shape.circleRadius!,
+        })
+      } else {
+        this.colorShapes[colorIndex].push(shape.vertices.map((vector) => Vector.clone(vector)))
+      }
     }
   }
 }
