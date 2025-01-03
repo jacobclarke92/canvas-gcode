80e51990f28fdc048e941ed0d1f7a859136c1663
Jacob Clarke <jacob@bigfish.tv>
2025-01-03 18:16:24 +1100

    Okay I need to stop working on Jan1

--- src/sketches/58_Genuary1.ts
+++ src/sketches/58_Genuary1.ts
@@ -6,10 +6,10 @@ import { Sketch } from '../Sketch'
 import type { Line } from '../types'
 import type { RGB } from '../utils/colorUtils'
 import { hexToRgb, rgbToHex } from '../utils/colorUtils'
-import { debugDot } from '../utils/debugUtils'
+import type { Bounds } from '../utils/geomUtils'
 import { isInBounds, lineIntersectsWithAny } from '../utils/geomUtils'
-import { radToDeg, randFloatRange, randIntRange } from '../utils/numberUtils'
-import { initPen, penUp, plotBounds, stopAndWigglePen } from '../utils/penUtils'
+import { randFloatRange, randIntRange } from '../utils/numberUtils'
+import { initPen, plotBounds } from '../utils/penUtils'
 import { seedRandom } from '../utils/random'
 import { BooleanRange } from './tools/Range'
 
@@ -24,15 +24,16 @@ const tendrilToLines = (tendril: Point[]): Line[] => {
 export default class Genuary1 extends Sketch {
   init() {
     this.addVar('seed', { initialValue: 3994, min: 1000, max: 5000, step: 1 })
-    this.addVar('gutter', { disableRandomize: true, initialValue: 5, min: 0, max: 100, step: 0.1 })
+    this.addVar('speedUp', { initialValue: 32, min: 1, max: 50, step: 1 })
+    this.addVar('gutter', { disableRandomize: true, initialValue: 3, min: 0, max: 100, step: 0.1 })
     // this.addVar('minSegLength', { initialValue: 2, min: 1, max: 24, step: 1 })
-    this.addVar('maxSegLength', { initialValue: 14, min: 1, max: 24, step: 1 })
-    this.addVar('maxTendrils', { initialValue: 100, min: 1, max: 1000, step: 1 })
-    this.addVar('angleVariance', { initialValue: Math.PI * 0.7, min: 0, max: Math.PI, step: 0.1 })
-    this.addVar('maxPlacementAttempts', { initialValue: 256, min: 1, max: 1000, step: 1 })
+    this.addVar('maxSegLength', { initialValue: 8, min: 1, max: 24, step: 1 })
+    this.addVar('maxTendrils', { initialValue: 160, min: 1, max: 1000, step: 1 })
+    this.addVar('angleVariance', { initialValue: 1.4, min: 0, max: Math.PI, step: 0.1 })
+    this.addVar('maxPlacementAttempts', { initialValue: 128, min: 1, max: 1000, step: 1 })
     this.addVar('preventOverlapsAfter', {
       disableRandomize: true,
-      initialValue: 3,
+      initialValue: 8,
       min: 1,
       max: 24,
       step: 1,
@@ -42,12 +43,16 @@ export default class Genuary1 extends Sketch {
       initialValue: true,
     })
     this.vs.debugColors = new BooleanRange({ disableRandomize: true, initialValue: false })
+    this.vs.drawFour = new BooleanRange({ disableRandomize: true, initialValue: true })
   }
 
   tendrils: Point[][] = []
   tendrilLines: Line[][] = []
   lastAngle = 0
   placementAttempts = 0
+  currentBounds: Bounds = [0, 0, 0, 0]
+  centerPoint = new Point()
+  drawCount = 0
 
   initDraw(): void {
     seedRandom(this.vars.seed)
@@ -59,85 +64,122 @@ export default class Genuary1 extends Sketch {
     // idea 1: city roads from above branching out from a central point
     // idea 2: a cityscape with backdrop of sunset and blocky clouds
 
-    this.tendrils = [[this.cp.clone()]]
+    this.ctx.strokeStyle = '#000'
+    this.drawCount = 0
+    this.resetDraw()
+  }
+
+  resetDraw(): void {
+    const { gutter } = this.vars
+    this.currentBounds = [0, this.cw, this.ch, 0]
+    this.centerPoint = this.cp.clone()
+    if (this.vs.drawFour.value) {
+      const qw = (this.cw - gutter * 3) / 2
+      const qh = (this.ch - gutter * 3) / 2
+      if (this.drawCount === 0) {
+        this.currentBounds = [0, this.cw / 2, this.ch / 2, 0]
+        this.centerPoint = new Point(gutter + qw / 2, gutter + qh / 2)
+      } else if (this.drawCount === 1) {
+        this.currentBounds = [0, this.cw, this.ch / 2, this.cw / 2]
+        this.centerPoint = new Point(gutter * 2 + qw + qw / 2, gutter + qh / 2)
+      } else if (this.drawCount === 2) {
+        this.currentBounds = [this.ch / 2, this.cw / 2, this.ch, 0]
+        this.centerPoint = new Point(gutter + qw / 2, gutter * 2 + qh + qh / 2)
+      } else if (this.drawCount === 3) {
+        this.currentBounds = [this.ch / 2, this.cw, this.ch, this.cw / 2]
+        this.centerPoint = new Point(gutter * 2 + qw + qw / 2, gutter * 2 + qh + qh / 2)
+      }
+    }
+
+    this.tendrils = [[this.centerPoint.clone()]]
     this.tendrilLines = []
     this.lastAngle = 0
     this.placementAttempts = 0
-    this.ctx.strokeStyle = '#000'
   }
 
   getAverageAngle(tendril: Point[]): number {
     const pt = new Point()
-    tendril.forEach((p) => pt.add(p.x - this.cx, p.y - this.cy))
+    tendril.forEach((p) => pt.add(p.x - this.centerPoint.x, p.y - this.centerPoint.y))
     return Math.atan2(pt.y, pt.x)
   }
 
   draw(increment: number): void {
     const {
+      speedUp,
       gutter,
       maxTendrils,
       angleVariance,
       maxPlacementAttempts,
-      minSegLength,
       maxSegLength,
       preventOverlapsAfter,
     } = this.vars
-    if (this.tendrils.length > maxTendrils) return
 
-    const tendril = this.tendrils[this.tendrils.length - 1]
+    for (let i = 0; i < speedUp; i++) {
+      if (this.tendrils.length > maxTendrils) {
+        if (this.vs.drawFour.value && this.drawCount < 3) {
+          this.drawCount++
+          this.resetDraw()
+        }
+        return
+      }
 
-    const averageAngle =
-      tendril.length < 2 ? randFloatRange(Math.PI, -Math.PI) : this.getAverageAngle(tendril)
-    const aimAngle = averageAngle + randFloatRange(angleVariance, -angleVariance)
-    // snap to 90 degree angles
-    let angle = (Math.round(aimAngle / (Math.PI / 2)) * (Math.PI / 2)) % (Math.PI * 2)
-    // prevent backtracking
-    if (angle === this.lastAngle)
-      angle = (angle + randFloatRange(1) > 0.5 ? Math.PI / 2 : -Math.PI / 2) % (Math.PI * 2)
-    this.lastAngle = angle
+      const tendril = this.tendrils[this.tendrils.length - 1]
 
-    const dist = randIntRange(maxSegLength, 1 /*minSegLength*/)
+      const averageAngle =
+        tendril.length < 2 ? randFloatRange(Math.PI, -Math.PI) : this.getAverageAngle(tendril)
+      const aimAngle = averageAngle + randFloatRange(angleVariance, -angleVariance)
+      // snap to 90 degree angles
+      let angle = (Math.round(aimAngle / (Math.PI / 2)) * (Math.PI / 2)) % (Math.PI * 2)
+      // prevent backtracking
+      if (angle === this.lastAngle)
+        angle = (angle + randFloatRange(1) > 0.5 ? Math.PI / 2 : -Math.PI / 2) % (Math.PI * 2)
+      this.lastAngle = angle
 
-    const prevPt = tendril[tendril.length - 1]
-    const nextPt = prevPt.clone().moveAlongAngle(angle, dist)
-    if (
-      !isInBounds(nextPt, [0, this.cw, this.ch, 0], gutter) ||
-      this.placementAttempts > maxPlacementAttempts
-    ) {
-      this.tendrilLines.push(tendrilToLines(tendril))
-      this.tendrils.push([this.cp.clone()])
-      this.placementAttempts = 0
-      return
-    }
+      const dist = randIntRange(maxSegLength, 1 /*minSegLength*/)
 
-    const selfLines = tendrilToLines(tendril.slice(0, tendril.length - 2))
-    if (
-      !!this.vs.preventSelfIntersection.value &&
-      selfLines.length > 0 &&
-      lineIntersectsWithAny([prevPt, nextPt], ...selfLines)
-    ) {
-      this.placementAttempts++
-      return
-    } else {
-      for (const tendrilLines of this.tendrilLines) {
-        if (lineIntersectsWithAny([prevPt, nextPt], ...tendrilLines.slice(preventOverlapsAfter))) {
-          this.placementAttempts++
-          return
+      const prevPt = tendril[tendril.length - 1]
+      const nextPt = prevPt.clone().moveAlongAngle(angle, dist)
+      if (
+        !isInBounds(nextPt, this.currentBounds, gutter) ||
+        this.placementAttempts > maxPlacementAttempts
+      ) {
+        this.tendrilLines.push(tendrilToLines(tendril))
+        this.tendrils.push([this.centerPoint.clone()])
+        this.placementAttempts = 0
+        return
+      }
+
+      const selfLines = tendrilToLines(tendril.slice(0, tendril.length - 2))
+      if (
+        !!this.vs.preventSelfIntersection.value &&
+        selfLines.length > 0 &&
+        lineIntersectsWithAny([prevPt, nextPt], ...selfLines)
+      ) {
+        this.placementAttempts++
+        return
+      } else {
+        for (const tendrilLines of this.tendrilLines) {
+          if (
+            lineIntersectsWithAny([prevPt, nextPt], ...tendrilLines.slice(preventOverlapsAfter))
+          ) {
+            this.placementAttempts++
+            return
+          }
         }
       }
-    }
 
-    // no overlaps:
-    tendril.push(nextPt)
-    if (!!this.vs.debugColors.value) {
-      const color = hexToRgb(colors[this.tendrils.length % colors.length]).map((c) =>
-        clamp(c + Math.round(Math.sin(tendril.length / 5) * 90), 0, 255)
-      )
-      this.ctx.strokeStyle = rgbToHex(color as RGB)
+      // no overlaps:
+      tendril.push(nextPt)
+      if (!!this.vs.debugColors.value) {
+        const color = hexToRgb(colors[this.tendrils.length % colors.length]).map((c) =>
+          clamp(c + Math.round(Math.sin(tendril.length / 5) * 90), 0, 255)
+        )
+        this.ctx.strokeStyle = rgbToHex(color as RGB)
+      }
+      this.ctx.beginPath()
+      this.ctx.moveTo(prevPt.x, prevPt.y)
+      this.ctx.lineTo(nextPt.x, nextPt.y)
+      this.ctx.stroke()
     }
-    this.ctx.beginPath()
-    this.ctx.moveTo(prevPt.x, prevPt.y)
-    this.ctx.lineTo(nextPt.x, nextPt.y)
-    this.ctx.stroke()
   }
 }
