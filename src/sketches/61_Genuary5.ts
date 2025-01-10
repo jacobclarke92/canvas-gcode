c3410300c5859e379b890a1aadca7edb08e14e8b
Jacob Clarke <jacob@bigfish.tv>
2025-01-10 11:38:35 +1100

    Forgot to commit Genuary5 updates the other day

--- src/sketches/61_Genuary5.ts
+++ src/sketches/61_Genuary5.ts
@@ -1,13 +1,9 @@
-import * as clipperLib from 'js-angusj-clipper/web'
-
 import Point from '../Point'
 import { Sketch } from '../Sketch'
-import { debugDot, debugText } from '../utils/debugUtils'
-import { perlin2, seedNoise } from '../utils/noise'
-import { randFloatRange, randInt, randIntRange } from '../utils/numberUtils'
-import { initPen, penUp, plotBounds, stopAndWigglePen } from '../utils/penUtils'
+import { seedNoise } from '../utils/noise'
+import { randFloatRange, randIntRange } from '../utils/numberUtils'
+import { initPen, plotBounds } from '../utils/penUtils'
 import { random, seedRandom } from '../utils/random'
-import Range, { BooleanRange } from './tools/Range'
 
 export default class IsometricGrid extends Sketch {
   // static generateGCode = false
@@ -18,7 +14,8 @@ export default class IsometricGrid extends Sketch {
     this.addVar('speedUp',{ name: 'speedUp', initialValue: 10, min: 1, max: 100, step: 1, disableRandomize: true }) // prettier-ignore
     this.addVar('seed', { name: 'seed', initialValue: 1000, min: 1000, max: 5000, step: 1 })
     this.addVar('gutter', { name: 'gutter', initialValue: 10, min: 1, max: 100, step: 1 })
-    this.addVar('shadeSpacing', { name: 'gutter', initialValue: 1, min: 0.5, max: 2, step: 0.1 })
+    this.addVar('shadeSpacing', { name: 'gutter', initialValue: 1.2, min: 0.5, max: 2, step: 0.1 })
+    this.addVar('shadeAttempts', { name: 'gutter', initialValue: 64, min: 0, max: 255, step: 1 })
 
     this.addVar('edgeLength', { name: 'edgeLength', initialValue: 10.8, min: 1, max: 100, step: 0.1 }) // prettier-ignore
     this.addVar('angle', {
@@ -30,19 +27,20 @@ export default class IsometricGrid extends Sketch {
     })
   }
 
-  get hSize() {
-    return Math.cos(this.vars.angle) * this.vars.edgeLength * 2
-  }
+  get hSize() { return Math.cos(this.vars.angle) * this.vars.edgeLength * 2 } // prettier-ignore
+  get vSize() { return Math.sin(this.vars.angle) * this.vars.edgeLength } // prettier-ignore
+  get xCols() { return Math.floor((this.cw - this.vars.gutter * 2) / this.hSize) } // prettier-ignore
+  get yCols() { return Math.floor((this.ch - this.vars.gutter * 2) / this.vSize) } // prettier-ignore
+  get offsetX() { return ((this.cw - this.vars.gutter * 2) % this.hSize) / 2 } // prettier-ignore
+  get offsetY() { return ((this.ch - this.vars.gutter * 2) % this.vSize) / 2 } // prettier-ignore
 
-  get vSize() {
-    return Math.sin(this.vars.angle) * this.vars.edgeLength
-  }
+  shadedCells: string[] = []
 
   getPtForCoords(x: number, y: number) {
     const { gutter } = this.vars
     return new Point(
-      gutter + x * this.hSize + (y % 2 === 0 ? 0 : this.hSize / 2),
-      gutter + y * this.vSize
+      gutter + this.offsetX + x * this.hSize + (y % 2 === 0 ? 0 : this.hSize / 2),
+      gutter + this.offsetY + y * this.vSize
     )
   }
 
@@ -53,23 +51,22 @@ export default class IsometricGrid extends Sketch {
     plotBounds(this)
     // isometric grid, go
 
-    const { gutter, edgeLength, angle } = this.vars
+    this.shadedCells = []
 
-    const maxX = Math.floor((this.cw - gutter * 2) / this.hSize)
-    const maxY = Math.floor((this.ch - gutter * 2) / this.vSize)
+    const { shadeAttempts, edgeLength, angle } = this.vars
 
-    for (let y = 0; y < maxY; y++) {
-      for (let x = 0; x < maxX; x++) {
+    for (let y = 0; y < this.yCols; y++) {
+      for (let x = 0; x < this.xCols; x++) {
         const pt = this.getPtForCoords(x, y)
         this.ctx.beginPath()
         if (y % 4 !== 2 && y !== 0) {
           this.ctx.moveTo(...pt.toArray())
           this.ctx.lineToRelativeAngle(-angle, edgeLength)
-          if (y % 4 === 1 && x === maxX - 1) {
+          if (y % 4 === 1 && x === this.xCols - 1) {
             this.ctx.lineToRelative(0, this.vSize * 2)
           }
         }
-        if (y % 4 !== 1 && y !== maxY - 1) {
+        if (y % 4 !== 1 && y !== this.yCols - 1) {
           this.ctx.moveTo(...pt.toArray())
           this.ctx.lineToRelativeAngle(angle, edgeLength)
         }
@@ -83,16 +80,39 @@ export default class IsometricGrid extends Sketch {
       }
     }
 
-    this.shadeCell(1, 3, 0.5)
-    this.shadeCell(2, 0, 1, Math.PI / 2 - angle)
+    // this.shadeCell(1, 3, 0.5)
+    // this.shadeCell(2, 0, 1, Math.PI / 2 - angle)
+    // this.shadeCell(2, 0, 1, Math.PI / 2 + angle)
 
-    // TODO: this properly
-    for (let i = 0; i < 20; i++) {
-      this.shadeCell(
-        randIntRange(maxX),
-        randIntRange(Math.floor(maxY / 4)) * 4,
-        randFloatRange(0.5, 1)
-      )
+    for (let i = 0; i < shadeAttempts; i++) {
+      let x = randIntRange(this.xCols - 1)
+      let y = 4 + randIntRange(Math.floor((this.yCols - 5) / 4)) * 4
+      if (random() > 0.5) {
+        x = (x + 1) % (this.xCols - 1)
+        y -= 1
+      }
+      const key = `${x},${y}`
+      if (this.shadedCells.includes(key)) continue
+      else this.shadedCells.push(key)
+      this.shadeCell(x, y, randFloatRange(0.5, 1))
+    }
+
+    for (let i = 0; i < shadeAttempts; i++) {
+      const x = randIntRange(this.xCols - 1)
+      const y = randIntRange(Math.floor((this.yCols - 1) / 4)) * 4
+      const key = `${x},${y}`
+      if (this.shadedCells.includes(key)) continue
+      else this.shadedCells.push(key)
+      this.shadeCell(x, y, randFloatRange(0.5, 1), Math.PI / 2 - angle)
+    }
+
+    for (let i = 0; i < shadeAttempts; i++) {
+      const x = 1 + randIntRange(this.xCols - 2)
+      const y = randIntRange(Math.floor((this.yCols - 1) / 4)) * 4
+      const key = `${x},${y}`
+      if (this.shadedCells.includes(key)) continue
+      else this.shadedCells.push(key)
+      this.shadeCell(x, y, randFloatRange(0.5, 1), Math.PI / 2 + angle)
     }
   }
 
