8e12c0c27bc809f776765f553d08b7b2da6bfdcb
Jacob Clarke <jacob@bigfish.tv>
2025-03-05 11:03:08 +0700

    Modified to add a bezier path for the strands to follow

--- src/sketches/73_FlowAround.tsx
+++ src/sketches/73_FlowAround.tsx
@@ -9,7 +9,7 @@ import { debugDot } from '../utils/debugUtils'
 import type { Circle } from '../utils/geomUtils'
 import { circleOverlapsCircles, getBezierPoints, pointInCircle } from '../utils/geomUtils'
 import { perlin2, seedNoise } from '../utils/noise'
-import { randFloatRange, randIntRange } from '../utils/numberUtils'
+import { clamp, randFloatRange, randIntRange } from '../utils/numberUtils'
 import { initPen, plotBounds } from '../utils/penUtils'
 import { seedRandom } from '../utils/random'
 import { BooleanRange } from './tools/Range'
@@ -19,12 +19,24 @@ class Strand {
   ctx: GCanvas
   pt: Point
   pts: Point[]
+  bezierPts: Point[]
   angle: number
-  constructor({ pt, ctx, angle }: { pt: Point; ctx: GCanvas; angle: number }) {
+  constructor({
+    pt,
+    ctx,
+    angle,
+    bezierPts,
+  }: {
+    pt: Point
+    ctx: GCanvas
+    angle: number
+    bezierPts: Point[]
+  }) {
     this.done = false
     this.ctx = ctx
     this.pt = pt
     this.pts = [pt.clone()]
+    this.bezierPts = bezierPts
     this.angle = angle
   }
 }
@@ -46,7 +58,7 @@ export default class FlowAround extends Sketch {
       initialValue: 25,
       min: 1,
       max: 200,
-      step: 1,
+      step: 0.1,
       disableRandomize: true,
     })
     this.addVar('perlinOffsetX', { initialValue: 0, min: 0, max: 250, step: 1 })
@@ -55,8 +67,16 @@ export default class FlowAround extends Sketch {
     this.addVar('regionRepelForce', { initialValue: 1.8, min: 0, max: 100, step: 0.1 })
     this.addVar('regionAvoidRadiusOffset', { initialValue: 1, min: 0.1, max: 2, step: 0.01 })
     this.addVar('initialAngleFade', { initialValue: 1.02, min: 1, max: 1.25, step: 0.001 })
+    this.addVar('initialAngleRange', {
+      initialValue: Math.PI * 2,
+      min: 0.001,
+      max: Math.PI * 2,
+      step: 0.001,
+    })
     this.addVar('speedTowardsEndPt', { initialValue: 0.5, min: 0, max: 1, step: 0.01 })
     this.addVar('speedThroughNoise', { initialValue: 0.5, min: 0, max: 5, step: 0.01 })
+    this.addVar('bezierIntensity', { initialValue: 120, min: 0, max: 500, step: 1 })
+    this.addVar('bezierSpeed', { initialValue: 0.5, min: 0, max: 2, step: 0.01 })
   }
 
   strandsDone = 0
@@ -71,7 +91,16 @@ export default class FlowAround extends Sketch {
     seedRandom(this.vars.seed)
     seedNoise(this.vars.seed)
 
-    const { gutterX, gutterY, numLines, numObstacles, obstacleMinSize, obstacleMaxSize } = this.vars
+    const {
+      gutterX,
+      gutterY,
+      numLines,
+      numObstacles,
+      obstacleMinSize,
+      obstacleMaxSize,
+      bezierIntensity,
+      initialAngleRange,
+    } = this.vars
 
     this.startPoint = new Point(this.cw * gutterX, this.ch * 0.5)
     this.endPoint = new Point(this.cw * (1 - gutterX), this.ch * 0.5)
@@ -96,10 +125,20 @@ export default class FlowAround extends Sketch {
     this.strands = []
     this.currentStrandIndex = 0
     for (let i = 0; i < numLines; i++) {
+      // const angle = randFloatRange(Math.PI, -Math.PI)
+      const angle = (i / numLines) * initialAngleRange - initialAngleRange / 2
+      const bezierPts = getBezierPoints(
+        this.startPoint,
+        this.startPoint.clone().moveAlongAngle(angle, bezierIntensity),
+        this.endPoint.clone().moveAlongAngle(Math.PI + angle, bezierIntensity),
+        this.endPoint,
+        50
+      )
       this.strands[i] = new Strand({
         ctx: this.ctx,
         pt: this.startPoint.clone(),
-        angle: randFloatRange(Math.PI, -Math.PI),
+        angle,
+        bezierPts,
       })
     }
   }
@@ -135,6 +174,7 @@ export default class FlowAround extends Sketch {
       initialAngleFade,
       speedThroughNoise,
       speedTowardsEndPt,
+      bezierSpeed,
     } = this.vars
 
     for (const strand of this.strands) {
@@ -150,9 +190,19 @@ export default class FlowAround extends Sketch {
       //   strand.pt.moveAlongAngle(trajectoryAngle, 1)
       // }
 
+      // const xPercent = (strand.pt.x - this.startPoint.x) / (this.endPoint.x - this.startPoint.x)
+      const xPercent = strand.pt.x / (this.cw - this.startPoint.x)
+      const bezierPtIndex = clamp(
+        Math.floor(xPercent * (strand.bezierPts.length - 1)),
+        0,
+        strand.bezierPts.length - 1
+      )
+      const bezierPt = strand.bezierPts[bezierPtIndex]
+
       strand.pt
         .moveAlongAngle(strand.angle, 1)
         .moveAlongAngle(noiseAngleForPos * Math.PI, speedThroughNoise)
+        .moveTowards(bezierPt, bezierSpeed)
         .moveTowards(this.endPoint, speedTowardsEndPt * Math.max(1, (100 - distFromEndPt) / 10))
 
       const closestObstacles = this.obstacles.sort(
@@ -173,19 +223,22 @@ export default class FlowAround extends Sketch {
         strand.pt.moveAway(pt, regionRepelForce * proximityToCenter)
       }
 
-      strand.pts.push(strand.pt.clone())
-      strand.angle /= initialAngleFade
-      this.ctx.strokeLine(strand.pts[strand.pts.length - 1], strand.pts[strand.pts.length - 2])
-      if (
-        strand.pt.x > this.cw * (1 - gutterX) ||
-        strand.pt.y < this.ch * gutterY ||
-        strand.pt.y > this.ch * (1 - gutterY) ||
-        strand.pts.length > 1000
-      ) {
-        strand.done = true
-        this.strandsDone++
-        if (this.strandsDone === this.strands.length) {
-          this.drawFinal()
+      const distFromLast = strand.pts[strand.pts.length - 1].distanceTo(strand.pt)
+      if (distFromLast > 1) {
+        strand.pts.push(strand.pt.clone())
+        strand.angle /= initialAngleFade
+        this.ctx.strokeLine(strand.pts[strand.pts.length - 1], strand.pts[strand.pts.length - 2])
+        if (
+          strand.pt.x > this.cw * (1 - gutterX) ||
+          strand.pt.y < this.ch * gutterY ||
+          strand.pt.y > this.ch * (1 - gutterY) ||
+          strand.pts.length > 2500
+        ) {
+          strand.done = true
+          this.strandsDone++
+          if (this.strandsDone === this.strands.length) {
+            this.drawFinal()
+          }
         }
       }
     }
